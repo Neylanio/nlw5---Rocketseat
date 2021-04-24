002961de449ff5d025bc203149bb06e2d75a4ed2
Neylanio Gomes Soares <neylanio.soares@mundiale.com.br>
2021-04-24 18:32:43 -0300

    feat: connect to sqlite dabatase and create method from settings

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import express from 'express'
+import './database'
+import 'reflect-metadata'
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/', (request, response) => {
-  return response.json({message: "ok"})
-})
-
-app.post('/:user', (request, response) => {
-  return response.json({
-   message: `usuario ${request.params.user} salvo com sucesso` 
-  })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('server is runnning on port 3333')
